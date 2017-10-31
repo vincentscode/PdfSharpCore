5c789144166639c44690cba19f7f95e70f0cd708
akgulebubekir <ebubekirakgul91@gmail.com>
2017-10-31 16:40:14 +0300

    ProgreessBar Renderer updated

--- PdfSharp.Xamarin.Forms/Renderers/PdfProgressBarRenderer.cs
+++ PdfSharp.Xamarin.Forms/Renderers/PdfProgressBarRenderer.cs
@@ -13,28 +13,23 @@ namespace PdfSharp.Xamarin.Forms.Renderers
 {
     [PdfRenderer(ViewType = typeof(ProgressBar))]
     public class PdfProgressBarRenderer : PdfRendererBase<ProgressBar>
-	{
-		public override void CreatePDFLayout(XGraphics page, ProgressBar progressBar, XRect bounds, double scaleFactor)
-		{
-			Color bgColor = progressBar.BackgroundColor;
-			if (bgColor == default(Color))
-				bgColor = Color.White;
-
-			Color barColor = Color.LightBlue;
+    {
+        public override void CreatePDFLayout(XGraphics page, ProgressBar progressBar, XRect bounds, double scaleFactor)
+        {
+            Color bgColor = progressBar.BackgroundColor != default(Color) ? progressBar.BackgroundColor : Color.LightGray;
+            Color barColor = Color.FromHex("#189DC4");
 
 
-			page.DrawRectangle(bgColor.ToXBrush(), bounds);
+            page.DrawRectangle(bgColor.ToXBrush(), bounds);
+
+            XRect progress = new XRect(bounds.X + scaleFactor,
+                                       bounds.Y + scaleFactor,
+                                       bounds.Width * progressBar.Progress,
+                                       bounds.Height - 2 * scaleFactor);
 
 
-			XRect progress = new XRect(bounds.X + scaleFactor,
-									   bounds.Y + scaleFactor,
-									   bounds.Width * progressBar.Progress,
-									   bounds.Height - 2 * scaleFactor);
-
-			page.DrawRectangle(barColor.ToXBrush(), progress);
-
-			//Draw borders
-			page.DrawRoundedRectangle(new XPen(Color.LightGray.ToXColor(), 2 * scaleFactor), bounds, new XSize(3 * scaleFactor, 3 * scaleFactor));
-		}
-	}
+            page.DrawRectangle(bgColor.ToXBrush(), bounds);
+            page.DrawRectangle(barColor.ToXBrush(), progress);
+        }
+    }
 }
