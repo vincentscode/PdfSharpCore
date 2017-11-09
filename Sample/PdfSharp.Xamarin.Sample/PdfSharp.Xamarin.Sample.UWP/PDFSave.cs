a1a0f33fb26cf801df9b90e1b82a9a7342653460
akgulebubekir <ebubekirakgul91@gmail.com>
2017-11-09 09:07:07 +0300

    Samples Updated

--- Sample/PdfSharp.Xamarin.Sample/PdfSharp.Xamarin.Sample.UWP/PDFSave.cs
+++ Sample/PdfSharp.Xamarin.Sample/PdfSharp.Xamarin.Sample.UWP/PDFSave.cs
@@ -10,11 +10,18 @@ using PdfSharp.Xamarin.Sample.UWP;
 [assembly: Xamarin.Forms.Dependency(typeof(PdfSave))]
 namespace PdfSharp.Xamarin.Sample.UWP
 {
-    public class PdfSave : IPdfSave
-    {
-        public void Save(PdfDocument doc, string fileName)
-        {
-            doc.Save(System.IO.Path.GetTempPath() + fileName);
-        }
-    }
+	public class PdfSave : IPdfSave
+	{
+		public void Save(PdfDocument doc, string fileName)
+		{
+			string path = System.IO.Path.GetTempPath() + fileName;
+
+			doc.Save(System.IO.Path.GetTempPath() + fileName);
+
+			global::Xamarin.Forms.Application.Current.MainPage.DisplayAlert(
+				title: "Success",
+				message: $"Your PDF generated and saved @ {path}",
+				cancel: "OK");
+		}
+	}
 }
--- Sample/PdfSharp.Xamarin.Sample/PdfSharp.Xamarin.Sample/MainPage.xaml.cs
+++ Sample/PdfSharp.Xamarin.Sample/PdfSharp.Xamarin.Sample/MainPage.xaml.cs
@@ -3,29 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using PdfSharp.Xamarin.Forms;
+
 using Xamarin.Forms;
 
 namespace PdfSharp.Xamarin.Sample
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
 
-        private void SinglePageButton_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new SinglePage());
-        }
+			picker.ItemsSource = new List<string>() { "Item 1", "Item 2", "Item 3" };
+			picker.SelectedIndex = 0;
+		}
 
-        private void ScrollPageButton_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new ScrollPage());
-        }
-        private void ListViewPageButton_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new ListViewPage());
-        }
-    }
+		private void GeneratePDF(object sender, EventArgs e)
+		{
+			var pdf = PDFManager.GeneratePDFFromView(mainGrid);
+
+			DependencyService.Get<IPdfSave>().Save(pdf, "SinglePage.pdf");
+		}
+	}
 }
