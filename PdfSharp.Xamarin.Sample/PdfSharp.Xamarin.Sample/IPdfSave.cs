f790c3649ea8cb9339b5e599d92aa94f8b038049
akgulebubekir <ebubekirakgul91@gmail.com>
2018-06-27 16:22:14 +0300

    Sample Project update & .Net Standards 1.4
    Sample project migrated to .Net Standards 1.4

--- PdfSharp.Xamarin.Sample/PdfSharp.Xamarin.Sample/IPdfSave.cs
+++ PdfSharp.Xamarin.Sample/PdfSharp.Xamarin.Sample/IPdfSave.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace PdfSharp.Xamarin.Sample
 {
-    public interface IPdfSave
-    {
-        void Save(PdfDocument doc,string fileName);
-    }
+	public interface IPdfSave
+	{
+		void Save(PdfDocument doc, string fileName);
+	}
 }
